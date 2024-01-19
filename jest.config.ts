29548368e05efff525ab0eb63fe71deb245d3000
Gabriel Bursi <gabriellbursii@gmail.com>
2024-01-18 21:44:29 -0300

    config jest

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'react-native',
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: [
+    'src/components/**/*.tsx',
+    'src/screens/**/*.tsx',
+    '!src/components/**/*.spec.tsx',
+    '!src/components/**/*.stories.tsx',
+    '!src/screens/**/*.spec.tsx',
+    '!src/**/*.spec.tsx',
+    '!src/types/**',
+    '!src/styles/**',
+    '!src/**/*.stories.tsx',
+    '!src/**/types.ts',
+    '!src/**/styles.ts',
+    '!src/components/index.ts',
+    '!src/screens/index.ts',
+  ],
+};
+
+export default config;
